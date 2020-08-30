c842f7a88c93a5904fa013a09605b0eeb77cde98
Rainer Selvet <rainerselvet@gmail.com>
2020-08-30 10:42:29 +0300

    updated urls

--- notion/getAssetUrl.js
+++ notion/getAssetUrl.js
@@ -1,5 +1,5 @@
 module.exports = (url, blockId) => {
-  const BASE = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://potion-api.now.sh"
+  const BASE = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://potion-psi.vercel.app"
 
   return `${BASE}/api/asset?url=${encodeURIComponent(url)}&blockId=${blockId}`
 }
